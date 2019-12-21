46331b1deb708abd8a76a1bd1195be813f78946e
knateghi <nateghikoby@gmail.com>
2019-12-20 19:21:32 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP123_s2019_Lesson11B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
